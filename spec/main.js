40780786afddfaf77a2375260a57c053d27fa04a
Peter Ajtai <peter.ajtai@gmail.com>
2013-04-27 23:41:04 -0700

    Build: 12 Branch: master  0.0.7 SHA: 2015017

--- spec/main.js
+++ spec/main.js
@@ -1,60 +1,78 @@
-
-describe("Function", function() {
-
-    var testExpression = true,
-        testDeclaration = true;
-
-    it("Function Declarations are not hoisted but defined for entire block", function() {
-
-        expect(typeof testDeclaration).toBe("function");
-        function testDeclaration() {};
-    });
-
-    it("Function expressions are hoisted", function() {
-
-        expect(typeof testExpression).toBe("undefined");
-        var testExpression = function() {};
-    });
-
-    it("arguments are passed by reference", function() {
-
-        var obj = { val : false };
-
-        function changeObj(objIn) {
-            objIn.val = true;
-        }
-
-        expect(obj.val).toEqual(false);
-        changeObj(obj);
-        expect(obj.val).toEqual(true);
-    });
-});
-
 describe("Object", function() {
 
     var obj = {};
 
-   it("objects have a constructor field", function() {
+    it("objects have a constructor field", function() {
 
        expect(obj.constructor).toBeDefined();
-   });
+    });
 
-   it("arrays can be differentiated from other objects using the constructor field", function() {
+    describe("Array", function() {
 
-       var arrs = [[], new Array(), Array.prototype.slice.call({}, 0)],
-           constr = function() {},
-           objs = [{}, new Object(), new constr()],
-           i;
+        it("The length of an array equals the highest index plus one", function() {
+            var arr = [];
 
-       for (i = 0; i < arrs.length; ++i) {
-           expect(arrs[i].constructor).toEqual(Array);
-           expect(arrs[i].constructor).not.toEqual(Object);
-       }
+            arr[99] = "test";
+            arr[-3] = "test";
+            expect(arr.length).toBe(100);
+        });
 
-       for (i = 0; i < objs.length; ++i) {
-           expect(objs[i].constructor).not.toEqual(Array);
-       }
-   });
+        it("arrays can be differentiated from other objects using the constructor field", function() {
+
+            var arrs = [[], new Array(), Array.prototype.slice.call({}, 0)],
+                constr = function() {},
+                objs = [{}, new Object(), new constr()],
+                i;
+
+            for (i = 0; i < arrs.length; ++i) {
+                expect(arrs[i].constructor).toEqual(Array);
+                expect(arrs[i].constructor).not.toEqual(Object);
+            }
+
+            for (i = 0; i < objs.length; ++i) {
+                expect(objs[i].constructor).not.toEqual(Array);
+            }
+        });
+
+        it("arrays can have negative indices", function() {
+
+            var arr =[];
+
+            arr[-1] = "tomato";
+            expect(arr[-1]).toEqual("tomato");
+        });
+    });
+
+    describe("Function", function() {
+
+        var testExpression = true,
+            testDeclaration = true;
+
+        it("Function Declarations are not hoisted but defined for entire block", function() {
+
+            expect(typeof testDeclaration).toBe("function");
+            function testDeclaration() {};
+        });
+
+        it("Function expressions are hoisted", function() {
+
+            expect(typeof testExpression).toBe("undefined");
+            var testExpression = function() {};
+        });
+
+        it("arguments are passed by reference", function() {
+
+            var obj = { val : false };
+
+            function changeObj(objIn) {
+                objIn.val = true;
+            }
+
+            expect(obj.val).toEqual(false);
+            changeObj(obj);
+            expect(obj.val).toEqual(true);
+        });
+    });
 
    describe("Object inheritance", function() {
 
@@ -80,6 +98,26 @@ describe("Object", function() {
            expect(obj2.test).toEqual("new");
        });
    });
+
+    describe("String", function() {
+        it("concatenating to a string leaves the original unchanged", function() {
+
+            var originalString = "original",
+                referenceString = originalString;
+
+            expect(originalString).toBe(referenceString);
+
+            referenceString += " and another string";
+
+            expect(originalString).toBe("original");
+            expect(referenceString).not.toBe(originalString);
+        });
+
+        it("string literal are automatically converted into string objects", function() {
+
+            expect("test".constructor).toBe(String);
+        });
+    });
 });
 
 describe("slice()", function() {
@@ -126,17 +164,34 @@ describe("slice()", function() {
 
 });
 
-describe("String", function() {
-   it("concatenating to a string leaves the original unchanged", function() {
+describe("splice()", function() {
 
-       var originalString = "original",
-           referenceString = originalString;
+    var input;
 
-       expect(originalString).toBe(referenceString);
+    beforeEach(function() {
 
-       referenceString += " and another string";
+        input = [0,1,2,3,4,5]
+    });
 
-       expect(originalString).toBe("original");
-       expect(referenceString).not.toBe(originalString);
-   });
+    it("splice returns the removed portion of the array", function() {
+
+        expect(input.splice(3,3)).toEqual([3,4,5]);
+    });
+
+    it("splice changes the original array", function() {
+
+        var input2 = input;
+
+        input.splice(3,3);
+
+        expect(input2).toBe(input);
+        expect(input).not.toEqual([0,1,2,3,4,5]);
+    });
+
+    it("splice can insert multiple elements as separate arguments into the array", function() {
+
+        input.splice(3,3,3,2,1,0);
+
+        expect(input).toEqual([0,1,2,3,2,1,0]);
+    });
 });
